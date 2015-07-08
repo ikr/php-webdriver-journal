15ccf1e5d031313ff5f5c2b2fdbfa053bf88cefa
Ivan Krechetov <ikr@ikr.su>
2015-07-08 16:23:09 +0200

    Implement Journal/screenshot()

--- src/WebDriverJournal/Journal.php
+++ src/WebDriverJournal/Journal.php
@@ -4,5 +4,15 @@ namespace WebDriverJournal;
 
 class Journal
 {
+    public function __construct($directoryFilesWriter)
+    {
+        $this->directoryFilesWriter = $directoryFilesWriter;
+    }
 
+    public function screenshot($pngBuffer)
+    {
+        $name = uniqid() . '.png';
+        $this->directoryFilesWriter->save($name, $pngBuffer);
+        $this->directoryFilesWriter->log('log.md', "![]($name)");
+    }
 }
--- tests/JournalTest.php
+++ tests/JournalTest.php
@@ -1,9 +1,52 @@
 <?php
 
+use Mockery as m;
+
 class JournalTest extends PHPUnit_Framework_TestCase
 {
-    public function testInstantiation()
+    protected function tearDown()
     {
-        $this->assertNotNull(new WebDriverJournal\Journal);
+        m::close();
+    }
+
+    public function testScreenshotSavesThePngBits()
+    {
+        $w = self::writer();
+        $w->shouldReceive('save')->once()->with(m::any(), 'png-bits');
+
+        $j = new WebDriverJournal\Journal($w);
+        $j->screenshot('png-bits');
+    }
+
+    public function testScreenshotGeneratesPngFileName()
+    {
+        $w = self::writer();
+        $w->shouldReceive('save')->once()->with('/^[a-z0-9]+\.png$/', m::any());
+
+        $j = new WebDriverJournal\Journal($w);
+        $j->screenshot('');
+    }
+
+    public function testScreenshotLogsToLogMd()
+    {
+        $w = self::writer();
+        $w->shouldReceive('log')->once()->with('log.md', m::any());
+
+        $j = new WebDriverJournal\Journal($w);
+        $j->screenshot('');
+    }
+
+    public function testScreenshotLogsTheMarkdownImageItem()
+    {
+        $w = self::writer();
+        $w->shouldReceive('log')->once()->with(m::any(), '/!\[.*\]\(.*\.png/');
+
+        $j = new WebDriverJournal\Journal($w);
+        $j->screenshot('');
+    }
+
+    private static function writer()
+    {
+        return m::mock('writer')->shouldIgnoreMissing();
     }
 }
