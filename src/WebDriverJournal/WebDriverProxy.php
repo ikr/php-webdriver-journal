f98d852e6823e2960d701cf4d8db2b2a71037cd4
Ivan Krechetov <ikr@ikr.su>
2015-07-09 08:39:30 +0200

    Implement WebDriverProxy

--- src/WebDriverJournal/WebDriverProxy.php
+++ src/WebDriverJournal/WebDriverProxy.php
@@ -4,5 +4,18 @@ namespace WebDriverJournal;
 
 class WebDriverProxy
 {
+    public function __construct($webDriver, $probe)
+    {
+        $this->webDriver = $webDriver;
+        $this->probe = $probe;
+    }
 
+    public function __call($methodName, $arguments)
+    {
+        $this->probe->run();
+        $result = call_user_func_array([$this->webDriver, $methodName], $arguments);
+        $this->probe->run();
+
+        return $result;
+    }
 }
--- tests/WebDriverProxyTest.php
+++ tests/WebDriverProxyTest.php
@@ -1,9 +1,43 @@
 <?php
 
+use Mockery as m;
+
 class WebDriverProxyTest extends PHPUnit_Framework_TestCase
 {
-    public function testInstantiation()
+    protected function tearDown()
     {
-        $this->assertNotNull(new WebDriverJournal\WebDriverProxy);
+        m::close();
+    }
+
+    /**
+     * @dataProvider proxiedMethods
+     */
+    public function testProbing($webDriverMethodName)
+    {
+        $driver = m::mock('driver');
+
+        $driver
+            ->shouldReceive($webDriverMethodName)
+            ->once()
+            ->with('foo-arg', 'bar-arg')
+            ->andReturn('le-result');
+
+        $probe = m::mock('probe');
+        $probe->shouldReceive('run')->twice();
+
+        $proxy = new WebDriverJournal\WebDriverProxy($driver, $probe);
+        $result = $proxy->$webDriverMethodName('foo-arg', 'bar-arg');
+
+        $this->assertSame('le-result', $result);
+    }
+
+    public static function proxiedMethods()
+    {
+        return [
+            ['findElement'],
+            ['findElements'],
+            ['executeScript'],
+            ['manage']
+        ];
     }
 }
